12d6c55b6999e99484a8d3f9816cd17c0bd0de48
lpdft <772346730@qq.com>
2020-08-11 23:04:08 +0800

    [ruyue] feat:click back or login button can back to MainActivity

--- app/src/main/java/com/example/hello/ConstrainActivity.java
+++ app/src/main/java/com/example/hello/ConstrainActivity.java
@@ -3,6 +3,8 @@ package com.example.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConstrainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ConstrainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constrain);
+
+        Button button1 = findViewById(R.id.login);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
